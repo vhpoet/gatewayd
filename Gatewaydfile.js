75e8f44e5d3a26e1b8ebbecd36ef688f9f6c4b29
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-21 08:55:39 -0700

    [FEATURE] Remove ripple behavior into plugin.

--- Gatewaydfile.js
+++ Gatewaydfile.js
@@ -1,5 +1,14 @@
+process.env.config = __dirname+'/config/config.json';
+const RipplePlugin = require('gatewayd-ripple-plugin');
 
 module.exports = function(gatewayd) {
+  var plugin = new RipplePlugin();
 
+  gatewayd.processes.add('./node_modules/gatewayd-ripple-plugin/processes/outgoing.js');
+  gatewayd.processes.add('./node_modules/gatewayd-ripple-plugin/processes/outgoing.js');
+  gatewayd.server.use('/ripple-simple', plugin.router);
+
+
+  plugin.remote.connect();
 }
 
