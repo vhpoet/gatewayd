30b2bcece210f9d6a04acd21ce75b3d20fbff389
Steven Zeiler <zeiler.steven@gmail.com>
2014-11-13 15:11:11 -0800

    [TASK] Add direction and real address to seeder

--- test/models/db_ripple_seed.js
+++ test/models/db_ripple_seed.js
@@ -3,8 +3,10 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var RippleTransactions = require(__dirname+'/../../').data.models.rippleTransactions;
+var RippleAddresses = require(__dirname+'/../../').data.models.rippleAddresses;
 var Chance = require('chance');
 var chance = new Chance();
+var rippleLib= require('ripple-lib');
 
 describe('RippleTransactions ', function() {
 
@@ -17,39 +19,65 @@ describe('RippleTransactions ', function() {
   });
 
   it('should successfully persist a ripple_transaction record with chance generated data', function() {
-    var chanceToAddressId = chance.integer({min: 1, max: 99999}),
-        chanceFromAddressId = chance.integer({min: 1, max: 99999}),
-        chanceToAmount = chance.floating({fixed: 2}),
+    var stateMap = ['incoming', 'outgoing', 'failed', 'pending', 'completed'];
+    var chanceToAmount = chance.floating({min: 1, fixed: 2}),
         chanceToCurrency = chance.currency().code,
-        chanceToIssuer = 'r' + chance.character({length: 34, alpha: true}),
-        chanceFromAmount = chance.floating({fixed: 2}),
+        toIssuer = rippleLib.Wallet.generate().address,
+        chanceFromAmount = chance.floating({min: 1, fixed: 2}),
         chanceFromCurrency = chance.currency().code,
-        chanceFromIssuer = 'r' + chance.character({length: 34, alpha: true}),
-        chanceInvoiceId = chance.hash({length: 32, casing: 'upper'});
+        fromIssuer = rippleLib.Wallet.generate().address,
+        chanceInvoiceId = chance.hash({length: 64, casing: 'upper'}),
+        chanceDirection = chance.bool() ? 'to-ripple' : 'from-ripple',
+        chanceState = stateMap[chance.integer({min: 0, max: 4})],
+        toAddressId, fromAddressId;
 
+    return RippleAddresses.findOrCreate({
+      type: 'independent',
+      managed: false,
+      address: toIssuer
+    }).then(function(address) {
+      toAddressId = address.id;
 
-    return RippleTransactions.create({
-      to_address_id: chanceToAddressId,
-      from_address_id: chanceFromAddressId,
-      to_amount: chanceToAmount,
-      to_currency: chanceToCurrency,
-      to_issuer: chanceToIssuer,
-      from_amount: chanceFromAmount,
-      from_currency: chanceFromCurrency,
-      from_issuer: chanceFromIssuer,
-      invoice_id: chanceInvoiceId
-    }).then(function(transaction) {
-        chai.assert.strictEqual(transaction.to_address_id, chanceToAddressId);
-        chai.assert.strictEqual(transaction.from_address_id, chanceFromAddressId);
-        chai.assert.strictEqual(transaction.to_amount, chanceToAmount);
-        chai.assert.strictEqual(transaction.to_currency, chanceToCurrency);
-        chai.assert.strictEqual(transaction.to_issuer, chanceToIssuer);
-        chai.assert.strictEqual(transaction.from_amount, chanceFromAmount);
-        chai.assert.strictEqual(transaction.from_currency, chanceFromCurrency);
-        chai.assert.strictEqual(transaction.from_issuer, chanceFromIssuer);
-        chai.assert.strictEqual(transaction.invoice_id, chanceInvoiceId);
-    }).error(function(error) {
+      return RippleAddresses.findOrCreate({
+        type: 'independent',
+        managed: false,
+        address: fromIssuer
+      }).then(function(address) {
+        fromAddressId = address.id;
+
+        return RippleTransactions.create({
+          to_address_id: toAddressId,
+          from_address_id: fromAddressId,
+          to_amount: chanceToAmount,
+          to_currency: chanceToCurrency,
+          to_issuer: toIssuer,
+          from_amount: chanceFromAmount,
+          from_currency: chanceFromCurrency,
+          from_issuer: fromIssuer,
+          invoice_id: chanceInvoiceId,
+          direction: chanceDirection,
+          state: chanceState
+        }).then(function(transaction) {
+          chai.assert.strictEqual(transaction.to_address_id, toAddressId);
+          chai.assert.strictEqual(transaction.from_address_id, fromAddressId);
+          chai.assert.strictEqual(transaction.to_amount, chanceToAmount);
+          chai.assert.strictEqual(transaction.to_currency, chanceToCurrency);
+          chai.assert.strictEqual(transaction.to_issuer, toIssuer);
+          chai.assert.strictEqual(transaction.from_amount, chanceFromAmount);
+          chai.assert.strictEqual(transaction.from_currency, chanceFromCurrency);
+          chai.assert.strictEqual(transaction.from_issuer, fromIssuer);
+          chai.assert.strictEqual(transaction.invoice_id, chanceInvoiceId);
+          chai.assert.strictEqual(transaction.direction, chanceDirection);
+          chai.assert.strictEqual(transaction.state, chanceState);
+        }).error(function(error) {
+          throw new Error(JSON.stringify(error));
+        });
+      }).error(function(error) {
         throw new Error(JSON.stringify(error));
+      });
+    }).error(function(error) {
+      throw new Error(JSON.stringify(error));
     });
   });
 });
+
